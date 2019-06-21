591e5f7140d293e8bb3a6c68855d3af275f8b927
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2019-06-21 14:30:22 +0100

    first

---
+++ cons.php
@@ -0,0 +1,57 @@
+<?php
+	$conn = mysqli_connect("localhost", "root", "", "easytrip");
+
+	$sql ="SELECT * FROM `data` WHERE `id_carrefour`='".$_POST['numc_send']."'";
+	$result = mysqli_query($conn, $sql);
+
+	if(mysqli_num_rows($result) > 0){
+		echo ("<img src='lib/img/consultation.jpg'>");
+		$row = mysqli_fetch_assoc($result) ;
+		echo "<table cellpadding='1' cellspacing='20' border='0'><tr><th>Voie a</th><td>  </td></tr>";	
+
+		echo "<tr><th>Densité de traffic</th><td>";
+		if ($row['v1']==="0")
+			echo "faible";
+		elseif ($row['v1']==="1") 
+	 		echo "moyenne";
+	 	elseif ($row['v1']==="2")
+	 		echo "forte";
+		echo "</td></tr>";
+
+		echo "<tr><th>Feux de couleur</th><td>";
+		if ($row['far']==="1")
+			echo "rouge";
+		elseif ($row['fao']==="1") 
+	 		echo "orange";
+	 	elseif ($row['fav']==="1")
+	 		echo "vert";
+		echo "</td></tr>";
+
+
+
+
+		echo "<tr><th>Voie b</th><td>  </td></tr>";	
+
+		echo "<tr><th>Densité de traffic</th><td>";
+		if ($row['v2']==="0")
+			echo "faible";
+		elseif ($row['v2']==="1") 
+	 		echo "moyenne";
+	 	elseif ($row['v2']==="2")
+	 		echo "forte";
+		echo "</td></tr>";
+
+		echo "<tr><th>Feux de couleur</th><td>";
+		if ($row['fbr']==="1")
+			echo "rouge";
+		elseif ($row['fbo']==="1") 
+	 		echo "orange";
+	 	elseif ($row['fbv']==="1")
+	 		echo "vert";
+		echo "</td></tr>";	
+	
+	 
+	}else {
+		echo "Carrefour n'existe pas";
+	}
+?>
---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+	$conn = mysqli_connect("localhost", "root", "", "easytrip");
+	$result = mysqli_query($conn, "SELECT * FROM carrefour");
+
+	$data = array();
+	while ($row = mysqli_fetch_object($result))
+	{
+	    array_push($data, $row);
+	}
+
+	echo json_encode($data);
+	exit();
+?>
